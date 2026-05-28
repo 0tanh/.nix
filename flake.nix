43e05222b8d4c81ed9e6ca450bee78fc369b45ed
felinefyi <uncia@feline.fyi>
2026-05-28 09:38:23 -0700

    add common inputs to flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,54 @@
 {
   inputs = {
+    ## PACKAGE CHANNELS ##
+    # Primary nixpkgs repository: use unstable (latest rolling) by default
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    # Pinned nixpkgs to most recent stable, will be injected via an overlay
+    # You may want to edit this pin to new stables periodically
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    ## PUBLIC INPUTS ##
+    # Declarative partitioning and formatting
     disko = {
       url = "github:nix-community/disko/latest";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Manage your user's $HOME in addition to the system
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    # Persist & link data into your ephemeral root at boot time
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
+    # Community-managed modular hardware configurations
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    # SOPS-based secrets management
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    ## PRIVATE INPUTS
+
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       disko,
+      home-manager,
+      impermanence,
       nixos-hardware,
+      sops-nix,
       ...
-    }:
+    }@inputs:
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
 
