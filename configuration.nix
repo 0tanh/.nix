aa5392101a918b70ce2b4d9e14d79e4f13b09b6f
felinefyi <uncia@feline.fyi>
2026-05-28 08:17:58 -0700

    feat: add basic nix settings

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,48 @@
   # Set your time zone.
   time.timeZone = "Africa/Johannesburg"; # replace with your TZ
 
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      randomizedDelaySec = "30min";
+      options = "--delete-older-than 7d --max-jobs auto --cores 0";
+    };
+    optimise = {
+      automatic = true;
+      dates = [ "03:00" ]; # Periodically optimize the store
+    };
+    settings = {
+      # See https://jackson.dev/post/nix-reasonable-defaults/
+      auto-optimise-store = true;
+      accept-flake-config = true;
+      download-buffer-size = 524288000;
+      connect-timeout = 60000;
+      log-lines = 25;
+      min-free = 128000000; # 128MB
+      max-free = 1000000000; # 1GB
+      experimental-features = lib.mkDefault "nix-command flakes"; # Enable flakes and new 'nix' command
+      # warn-dirty = false;
+      # allow-import-from-derivation = true;
+      trusted-users = [
+        "@wheel"
+        "root"
+        "betty"
+      ];
+      builders-use-substitutes = true;
+      fallback = true; # Don't hard fail if a binary cache isn't available, since some systems roam
+      substituters = [
+        "https://cache.nixos.org" # Official global cache
+        "https://nix-community.cachix.org" # Community packages
+      ];
+      extra-substituters = [
+       "https://nix-community.cachix.org" # Nix community Cachix server
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
