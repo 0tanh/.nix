d232a967877f564c91fef4426b6c89f41eaf2b14
felinefyi <uncia@feline.fyi>
2026-05-28 12:19:01 -0700

    add scanPaths to lib

--- lib/default.nix
+++ lib/default.nix
@@ -10,5 +10,23 @@
     # Use path relative to the root of the flake
     # Usage: relativeToRoot "modules/nixos/directoryOrFileDotNix"
     relativeToRoot = lib.path.append ../.;
+
+    # Scan all paths in the given directory, returning a list of directories and *.nix files,
+    # not including 'default.nix'. Useful within a directory's 'default.nix' with
+    # `imports = scanPaths ./.;` as a way to automatically pull in all other files in a directory.
+    scanPaths =
+      path:
+      builtins.map (f: (path + "/${f}")) (
+        builtins.attrNames (
+          lib.attrsets.filterAttrs (
+            path: _type:
+            (_type == "directory") # include directories
+            || (
+              (path != "default.nix") # ignore default.nix
+              && (lib.strings.hasSuffix ".nix" path) # include .nix files
+            )
+          ) (builtins.readDir path)
+        )
+      );
   };
 }
