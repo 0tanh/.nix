c1d58d447db5bc06e36d70f7f36bbf86ab0d0fe7
0tanh <0tanh@git.feline.fyi>
2026-05-31 21:36:41 +0200

    update impermanence rules
    add Windows to home persist
    remove sops key since it's auto-generated

--- modules/nixos/home/impermanence.nix
+++ modules/nixos/home/impermanence.nix
@@ -16,6 +16,7 @@
       "Pictures"
       "Documents"
       "Videos"
+      "Windows"
 
       {
         directory = ".gnupg";
--- modules/nixos/system/impermanance.nix
+++ modules/nixos/system/impermanance.nix
@@ -56,12 +56,13 @@
     ];
     files = [
       "/etc/machine-id"
-      {
-        file = "/etc/sops/age/keys.txt";
-        parentDirectory = {
-          mode = "u=rwx,g=,o=";
-        };
-      }
+      # SOPS module already handles generating this
+      # {
+      #   file = "/etc/sops/age/keys.txt";
+      #   parentDirectory = {
+      #     mode = "u=rwx,g=,o=";
+      #   };
+      # }
       {
         file = "/etc/ssh/ssh_host_ed25519_key";
         parentDirectory = {
