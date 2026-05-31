cef317e5e12a5e25eb685f470cc09901843e9116
0tanh <0tanh@git.feline.fyi>
2026-05-31 21:29:24 +0200

    fix: need sudo to rm /etc files

--- pkgs/clean-problem-files/default.nix
+++ pkgs/clean-problem-files/default.nix
@@ -14,9 +14,9 @@ writeShellApplication {
   text = ''
     echo "Deleting orphaned .hm-backup files & problematic files for persisting ..."
 
-    rm -f /etc/machine-id
-    rm -f /etc/sops/age/keys.txt
-    rm -f /etc/ssh/ssh_host_ed25519_key
+    sudo rm -f /etc/machine-id
+    sudo rm -f /etc/sops/age/keys.txt
+    sudo rm -f /etc/ssh/ssh_host_ed25519_key
 
     rm -f /home/betty/.config/zsh/.zsh_history.hm-backup
   '';
