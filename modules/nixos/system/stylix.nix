82a504f99388e5bf35c2ec431fc4b9ac0556e05c
0tanh <0tanh@git.feline.fyi>
2026-06-08 01:56:55 +0200

    added more backgrounds ++ set stylix theme to originate from one of them

--- modules/nixos/system/stylix.nix
+++ modules/nixos/system/stylix.nix
@@ -14,7 +14,7 @@
     targets.kmscon.enable = false;
     # https://nix-community.github.io/stylix/configuration.html
     # Automatically set wallpaper, and generate a system colorscheme pallete using the folloing image.
-    image = ../../../assets/img/purple-lake.png;
+    image = ../../../assets/img/aesthetic_bg.png;
     # View the palette at /etc/stylix/palette.html
     polarity = "dark";
   };
