fa168644295e3be89967b2c3b2f764a7c58ec3b4
0tanh <0tanh@git.feline.fyi>
2026-06-08 03:44:42 +0200

    more idiomatic package management

--- modules/nixos/home/packages.nix
+++ modules/nixos/home/packages.nix
@@ -12,43 +12,55 @@
   #
   # as of 06/06/2026 the recommnended workflow is to use the
   # ../../../assets/submodule/dotfiles/ directory to manage these files
-  home.packages = with pkgs; [
-    # tuxedo has not been added to the lastest stable version of
-    # nixpkgs and needs to be pulled in from the less stable
-    # main branch
-    pkgs.bleeding.tuxedo
+  home.packages =
+    with pkgs;
+    [
 
-    inkscape
-    python314
-    telegram-desktop
-    swaybg
-    vlc
-    zeal
+      python314
+      mpv
+      telegram-desktop
+      swaybg
+      vlc
+      zeal
 
-    # Reaper for audio stuff
-    reaper
-    reaper-reapack-extension
-    reaper-sws-extension
-    yabridge
+      # Reaper for audio stuff
+      reaper
+      reaper-reapack-extension
+      reaper-sws-extension
+      yabridge
 
-    # nvim language servers & formatters
-    bash-language-server
-    docker-compose-language-service
-    dockerfile-language-server
-    jdt-language-server
-    just-lsp
-    lua-language-server
-    nixd
-    nixfmt
-    prettier
-    python3Packages.python-lsp-server
-    ruff
-    rustfmt
-    stylua
-    svelte-language-server
-    terraform-ls
-    vscode-langservers-extracted
-    yaml-language-server
-  ];
+      # nvim language servers & formatters
+      bash-language-server
+      docker-compose-language-service
+      dockerfile-language-server
+      jdt-language-server
+      just-lsp
+      lua-language-server
+      nixd
+      nixfmt
+      prettier
+      python3Packages.python-lsp-server
+      ruff
+      rustfmt
+      stylua
+      svelte-language-server
+      terraform-ls
+      vscode-langservers-extracted
+      yaml-language-server
+    ]
+
+    # these packages are from a stable version of
+    # nixpkgs. Inkscape is here to reduce its build time.
+    ++ (with pkgs.stable; [
+      inkscape
+    ])
+
+    # these packages are being pulled from the latest version of
+    # nixpkgs. They might be less stable.
+    ++ (with pkgs.bleeding; [
+      tuxedo
+    ])
+
+  ;
 
 }
