4e4944ce38a0cfd3446d9160a7753a0c5c8e78b2
0tanh <0tanh@git.feline.fyi>
2026-06-08 03:51:52 +0200

    fixing blur

--- modules/nixos/home/mango.nix
+++ modules/nixos/home/mango.nix
@@ -178,7 +178,7 @@ in
       ##################
       # https://mangowc.vercel.app/docs/configuration/monitors
       # monitorrule=name,mfact,nmaster,layout,transform,scale,x,y,width,height,refreshrate
-      monitorrule=name:^eDP-1$,scale:0.8,x:0,y:0,width:1440,height:900,refresh:60
+      monitorrule=name:^eDP-1$,scale:1,x:0,y:0,width:1440,height:900,refresh:60
 
       # input
       repeat_rate=20
