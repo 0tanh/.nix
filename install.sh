1c5965143b0fe975c480e63afa7a2c63241e7f68
felinefyi <uncia@feline.fyi>
2026-06-14 15:50:34 -0700

    feat: overhaul install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
-NIX_CONFIG="access-tokens = github.com=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" nix --experimental-features 'nix-command flakes' run github:nix-community/nixos-anywhere -- --flake .#lily --target-host root@192.168.10.141
+## Installl the flake 'lily' onto the remote device.
+
+# Get some info about the environment
+TEMP=$(mktemp -d) # Create tmpdir
+USER=$(whoami) # Get username
+
+# Set up SSH key to be copied after install
+install -d -m755 "$HOME/home/${USER}/.ssh"
+cp /home/${USER}/.ssh/id_ed25519 "${TEMP}/home/${USER}/.ssh/id_ed25519"
+chmod 600 "${TEMP}/home/${USER}/.ssh/id_ed25519"
+
+# Perform the install
+# TODO: take flake name and host as args $1 and $2
+NIX_CONFIG="access-tokens = github.com=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" \
+  nix --experimental-features 'nix-command flakes' \
+  run github:nix-community/nixos-anywhere -- \
+  --extra-files "${TEMP}" \
+  --flake .#lily \
+  --target-host root@192.168.10.141
+
+# Clean up tmpdir
+rm -rf "${TEMP}"
+
+# Remove known_hosts entry, it will have changed if we try to connect after install
+sed -ie '/^192\.168\.10\.141/d' /home/${USER}/.ssh/known_hosts
+
+echo "Success!"
