ab2266b12b9fe16717d38663f7177fe4581b1c93
felinefyi <uncia@feline.fyi>
2026-06-15 17:07:35 -0700

    feat: add drybuild alias

--- modules/nixos/home/zsh.nix
+++ modules/nixos/home/zsh.nix
@@ -172,6 +172,7 @@
       # dot = "cd ~/.nix/res/sub/dotfiles/";
       rebuild = "cd ~/.nix && treefmt ~/.nix && ${pkgs.clean-problem-files}/bin/clean-problem-files && nh os switch ~/.nix";
       update = "cd ~/.nix && treefmt ~/.nix && nix flake update secrets && nix flake update dotfiles && ${pkgs.clean-problem-files}/bin/clean-problem-files && nh os switch ~/.nix";
+      drybuild = "cd ~/.nix && treefmt ~/.nix && ${pkgs.clean-problem-files}/bin/clean-problem-files && nh os boot ~/.nix";
 
       # git
       ga = "git add";
