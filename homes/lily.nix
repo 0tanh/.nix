20f986c96b08a2e3acc6347f7c4e26dabc5facfd
0tanh <0tanh@git.feline.fyi>
2026-07-23 22:31:54 +0200

    feat: added lily to homes

--- homes/lily.nix
+++ homes/lily.nix
@@ -13,6 +13,7 @@
     ../modules/nixos/home/combos/audio-work.nix
     ../modules/nixos/home/combos/dev-work.nix
     ../modules/nixos/home/combos/minimal.nix
+    ../modules/nixos/home/combos/security-research.nix
     # The current desktop environment
     ../modules/nixos/home/combos/mango-quickshell.nix
     # Standard modules composed for this home manager.
