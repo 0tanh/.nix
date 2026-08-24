d23bcdcd380067eb5d597b19dcfc888313d50b96
0tanh <0tanh@git.feline.fyi>
2026-08-24 07:16:55 +0200

    niri: more configuration

--- modules/nixos/home/desktop-env/niri.nix
+++ modules/nixos/home/desktop-env/niri.nix
@@ -5,9 +5,30 @@
   config,
   ...
 }:
+
+let
+  quickshell-source = "${inputs.dotfiles}/quickshell";
+  niri-source = "${inputs.dotfiles}/niri";
+in
 {
   # This configuration uses the niri window manager
   programs.niri = {
     enable = true;
+    settings = {
+      # prefer-no-csd = true;
+      # input.keyboard.xkb = {
+      #   layout = "us";
+      #  variant = "intl";
+      # };
+      layout = {
+        gaps = 4;
+        focus-ring = {
+          #  enable = true;
+          width = 2;
+          #  active.color = "#${config.colorScheme.palette.base0D}ff";
+          #  inactive.color = "#${config.colorScheme.palette.base03}ff";
+        };
+      };
+    };
   };
 }
