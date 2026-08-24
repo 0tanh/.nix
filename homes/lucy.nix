9cc1893871d7720e5deb2787feb0ca3a92bba02e
0tanh <0tanh@git.feline.fyi>
2026-08-24 07:16:42 +0200

    stylix: enabled for lucy

--- homes/lucy.nix
+++ homes/lucy.nix
@@ -27,7 +27,7 @@
     # ../modules/nixos/home/neovim.nix
     ../modules/nixos/home/packages.nix
     ../modules/nixos/home/pet.nix
-    # ../modules/nixos/home/spicetify.nix
+    ../modules/nixos/home/spicetify.nix
     ../modules/nixos/home/ssh.nix
     ../modules/nixos/home/tmux.nix
     ../modules/nixos/home/vars.nix
