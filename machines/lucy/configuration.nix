c073ecf110a27d52436bae6cbeb1460ea6ee8e09
0tanh <0tanh@git.feline.fyi>
2026-09-02 11:46:22 +0200

    chore: dead kde in lucy

--- machines/lucy/configuration.nix
+++ machines/lucy/configuration.nix
@@ -39,10 +39,6 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  # services.desktopManager.plasma6.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "za";
@@ -67,11 +63,6 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  };
-  # Janky kde add
-  services = {
-    desktopManager.plasma6.enable = true;
-    displayManager.plasma-login-manager.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
