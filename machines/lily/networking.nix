873ef60df69152564e577ce06bbc63eb94e55ae2
0tanh <0tanh@git.feline.fyi>
2026-09-23 16:47:28 +0200

    lily: enabled nat

--- machines/lily/networking.nix
+++ machines/lily/networking.nix
@@ -24,7 +24,10 @@
         prefixLength = 24;
       }
     ];
-
+    # Allow Network Access Traversal
+    nat = {
+      enable = true;
+    };
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
